27360a3269f6a89162619be7d965878198551db4
Japheth Gado <japhethgado@gmail.com>
2023-01-31 17:02:23 -0500

    Updated

--- ephod/models.py
+++ ephod/models.py
@@ -1,1 +1,248 @@
-"""Modular classes for building and running EpHod modelAuthor: Japheth Gado"""import subprocessimport osimport torchimport torch.nn as nnfrom torch.nn.parallel import DataParallelimport esmimport ephod.utils as utilsdef download_models():    '''Download saved models (EpHod and AAC-SVR)'''    glink = 'https://drive.google.com/drive/folders/'\            '138cnx4hFrzNODGK6A_yd9wo7WupKpSjI?usp=share_link/'    cmd = f"gdown --folder {glink}"    _ = subprocess.call(cmd, shell=True) # Download model from google drive     cmd = "mv -f ./saved_models ./ephod/"    _ = subprocess.call(cmd, shell=True)                        def torchActivation(activation='elu'):    '''Return an activation function from torch.nn'''    if activation == 'relu':        return nn.ReLU()    elif activation == 'leaky_relu':        return nn.LeakyReLU()    elif activation == 'elu':        return nn.ELU()    elif activation == 'selu':        return nn.SELU()        elif activation == 'gelu':        return nn.GELU()        def count_parameters(model):    '''Return a count of parameters and tensor shape of PyTorch model'''         counted = {}    total = 0    for name, param in model.named_parameters():        if param.requires_grad:            count = param.numel()            total += count            counted[name] = count    counted['FULL_MODEL'] = total    return counted    class ResidualDense(nn.Module):    '''A single dense layer with residual connection'''        def __init__(self, dim=2560, dropout=0.1, activation='elu', random_seed=0):                super(ResidualDense, self).__init__()        _ = torch.manual_seed(random_seed)        self.dense = nn.Linear(dim, dim)        self.batchnorm = nn.BatchNorm1d(dim)        self.activation = torchActivation(activation)        self.dropout = nn.Dropout(dropout)    def forward(self, x):        x0 = x        x = self.dense(x)        x = self.batchnorm(x)        x = self.activation(x)                x = self.dropout(x)        x = x0 + x                return xclass LightAttention(nn.Module):    '''Convolution model with attention to learn pooled representations from embeddings'''    def __init__(self, dim=1280, kernel_size=7, random_seed=0):                super(LightAttention, self).__init__()        _ = torch.manual_seed(random_seed)                samepad = kernel_size // 2        self.values_conv = nn.Conv1d(dim, dim, kernel_size=kernel_size, padding=samepad)        self.weights_conv = nn.Conv1d(dim, dim, kernel_size=kernel_size, padding=samepad)        self.softmax = nn.Softmax(dim=-1)            def forward(self, x, mask=None):        if mask is None:            mask = torch.ones(x.shape[0], x.shape[2], dtype=torch.int32)  # Don't mask out        values = self.values_conv(x)        values = values.masked_fill(mask[:,None,:]==0, -1e-6)        weights = self.weights_conv(x)        weights = weights.masked_fill(mask[:,None,:]==0, -1e-6)        weights = self.softmax(weights)        x_sum = torch.sum(values * weights, dim=-1) # Attention-weighted pooling        x_max, _ = torch.max(values, dim=-1) # Max pooling        x = torch.cat([x_sum, x_max], dim=1)                return x, weights            class ResidualLightAttention(nn.Module):    '''Model consisting of light attention followed by residual dense layers'''        def __init__(self, dim=1280, kernel_size=9, dropout=0.5,                 activation='relu', res_blocks=4, random_seed=0):        super(ResidualLightAttention, self).__init__()        torch.manual_seed(random_seed)        self.light_attention = LightAttention(dim, kernel_size, random_seed)        self.batchnorm = nn.BatchNorm1d(2 * dim)                        self.dropout = nn.Dropout(dropout)                self.residual_dense = nn.ModuleList()                for i in range(res_blocks):            self.residual_dense.append(                ResidualDense(2 * dim, dropout, activation, random_seed)                )        self.output = nn.Linear(2 * dim, 1)                    def forward(self, x, mask=None):        x, weights = self.light_attention(x, mask)        x = self.batchnorm(x)        x = self.dropout(x)        for layer in self.residual_dense:            x = layer(x)        y = self.output(x).flatten()                return [y, x, weights]class EpHodModel():        def __init__(self):        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'        if self.device != 'cuda':            print('WARNING: You are not using a cpu which will be very slow.')        self.esm1v_model, self.esm1v_batch_converter = self.load_ESM1v_model()        self.rlat_model = self.load_RLAT_model()        _ = self.esm1v_model.eval()        _ = self.rlat_model.eval()                def load_ESM1v_model(self):        '''Return pretrained ESM1v model weights and batch converter'''                model, alphabet = esm.pretrained.esm1v_t33_650M_UR90S_1()        model = model.to(self.device)        batch_converter = alphabet.get_batch_converter()                return model, batch_converter            def get_ESM1v_embeddings(self, accs, seqs):        '''Return per-residue embeddings (padded) for protein sequences from ESM1v model'''        seqs = [utils.replace_noncanonical(seq, 'X') for seq in seqs]        data = [(accs[i], seqs[i]) for i in range(len(accs))]        batch_labels, batch_strs, batch_tokens = self.esm1v_batch_converter(data)        batch_tokens = batch_tokens.to(device=self.device, non_blocking=True)        emb = self.esm1v_model(batch_tokens, repr_layers=[33], return_contacts=False)        emb = emb["representations"][33]        emb = emb.transpose(2,1) # From (batch, seqlen, features) to (batch, features, seqlen)        return emb            def load_RLAT_model(self):        '''Return fine-tuned residual light attention top model'''        # Path to RLAT model        this_dir, this_filename = os.path.split(__file__)        params_path = os.path.join(this_dir, 'saved_models', 'RLAT', 'params.json')        rlat_path = os.path.join(this_dir, 'saved_models', 'RLAT', 'RLAT.pt')                # Download RLAT model from google drive if not in path        if not os.path.exists(rlat_path):            _ = download_models()                # Load RLAT model from path        checkpoint = torch.load(rlat_path, map_location=self.device)        params = utils.read_json(params_path)                model = ResidualLightAttention(**params)        model = DataParallel(model)        model.load_state_dict(checkpoint['model_state_dict'], strict=True)        return model        def batch_predict(self, accs, seqs):        '''Predict pHopt with EpHod on a batch of sequences'''                emb_esm1v = self.get_ESM1v_embeddings(accs, seqs)        maxlen = emb_esm1v.shape[-1]        masks = [[1] * len(seqs[i]) + [0] * (maxlen - len(seqs[i])) \                 for i in range(len(seqs))]        masks = torch.tensor(masks, dtype=torch.int32)        output = self.rlat_model(emb_esm1v, masks) # (ypred, emb_ephod, attention_weights)                return output            
+"""Modular classes for building and running EpHod model
+Author: Japheth Gado
+"""
+
+
+
+
+import subprocess
+import os
+
+import torch
+import torch.nn as nn
+from torch.nn.parallel import DataParallel
+import esm
+
+import ephod.utils as utils
+
+
+
+
+
+def download_models():
+    '''Download saved models (EpHod and AAC-SVR)'''
+
+    glink = 'https://drive.google.com/drive/folders/'\
+            '138cnx4hFrzNODGK6A_yd9wo7WupKpSjI?usp=share_link/'
+    cmd = f"gdown --folder {glink}"
+    _ = subprocess.call(cmd, shell=True) # Download model from google drive 
+    cmd = "mv -f ./saved_models ./ephod/"
+    _ = subprocess.call(cmd, shell=True)
+    
+    
+    
+    
+    
+    
+def torchActivation(activation='elu'):
+    '''Return an activation function from torch.nn'''
+
+    if activation == 'relu':
+        return nn.ReLU()
+
+    elif activation == 'leaky_relu':
+        return nn.LeakyReLU()
+
+    elif activation == 'elu':
+        return nn.ELU()
+
+    elif activation == 'selu':
+        return nn.SELU()
+    
+    elif activation == 'gelu':
+        return nn.GELU()
+        
+
+
+
+
+
+def count_parameters(model):
+    '''Return a count of parameters and tensor shape of PyTorch model''' 
+    
+    counted = {}
+    total = 0
+    for name, param in model.named_parameters():
+        if param.requires_grad:
+            count = param.numel()
+            total += count
+            counted[name] = count
+    counted['FULL_MODEL'] = total
+
+    return counted
+    
+
+
+
+
+
+class ResidualDense(nn.Module):
+    '''A single dense layer with residual connection'''
+    
+    def __init__(self, dim=2560, dropout=0.1, activation='elu', random_seed=0):
+        
+        super(ResidualDense, self).__init__()
+        _ = torch.manual_seed(random_seed)
+        self.dense = nn.Linear(dim, dim)
+        self.batchnorm = nn.BatchNorm1d(dim)
+        self.activation = torchActivation(activation)
+        self.dropout = nn.Dropout(dropout)
+
+
+    def forward(self, x):
+
+        x0 = x
+        x = self.dense(x)
+        x = self.batchnorm(x)
+        x = self.activation(x)        
+        x = self.dropout(x)
+        x = x0 + x
+        
+        return x
+
+
+
+
+
+
+class LightAttention(nn.Module):
+    '''Convolution model with attention to learn pooled representations from embeddings'''
+
+    def __init__(self, dim=1280, kernel_size=7, random_seed=0):
+        
+        super(LightAttention, self).__init__()
+        _ = torch.manual_seed(random_seed)        
+        samepad = kernel_size // 2
+        self.values_conv = nn.Conv1d(dim, dim, kernel_size=kernel_size, padding=samepad)
+        self.weights_conv = nn.Conv1d(dim, dim, kernel_size=kernel_size, padding=samepad)
+        self.softmax = nn.Softmax(dim=-1)
+    
+    
+    def forward(self, x, mask=None):
+        if mask is None:
+            mask = torch.ones(x.shape[0], x.shape[2], dtype=torch.int32)  # Don't mask out
+        values = self.values_conv(x)
+        values = values.masked_fill(mask[:,None,:]==0, -1e-6)
+        weights = self.weights_conv(x)
+        weights = weights.masked_fill(mask[:,None,:]==0, -1e-6)
+        weights = self.softmax(weights)
+        x_sum = torch.sum(values * weights, dim=-1) # Attention-weighted pooling
+        x_max, _ = torch.max(values, dim=-1) # Max pooling
+        x = torch.cat([x_sum, x_max], dim=1)
+        
+        return x, weights
+    
+    
+    
+
+
+
+class ResidualLightAttention(nn.Module):
+    '''Model consisting of light attention followed by residual dense layers'''
+    
+    def __init__(self, dim=1280, kernel_size=9, dropout=0.5,
+                 activation='relu', res_blocks=4, random_seed=0):
+
+        super(ResidualLightAttention, self).__init__()
+        torch.manual_seed(random_seed)
+        self.light_attention = LightAttention(dim, kernel_size, random_seed)
+        self.batchnorm = nn.BatchNorm1d(2 * dim)                
+        self.dropout = nn.Dropout(dropout)        
+        self.residual_dense = nn.ModuleList()        
+        for i in range(res_blocks):
+            self.residual_dense.append(
+                ResidualDense(2 * dim, dropout, activation, random_seed)
+                )
+        self.output = nn.Linear(2 * dim, 1)
+        
+        
+    def forward(self, x, mask=None):
+
+        x, weights = self.light_attention(x, mask)
+        x = self.batchnorm(x)
+        x = self.dropout(x)
+        for layer in self.residual_dense:
+            x = layer(x)
+        y = self.output(x).flatten()
+        
+        return [y, x, weights]
+
+
+
+
+
+
+class EpHodModel():
+    
+    def __init__(self):
+
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        if self.device != 'cuda':
+            print('WARNING: You are not using a cpu which will be very slow.')
+        self.esm1v_model, self.esm1v_batch_converter = self.load_ESM1v_model()
+        self.rlat_model = self.load_RLAT_model()
+        _ = self.esm1v_model.eval()
+        _ = self.rlat_model.eval()
+        
+    
+    def load_ESM1v_model(self):
+        '''Return pretrained ESM1v model weights and batch converter'''
+        
+        model, alphabet = esm.pretrained.esm1v_t33_650M_UR90S_1()
+        model = model.to(self.device)
+        batch_converter = alphabet.get_batch_converter()
+        
+        return model, batch_converter
+    
+    
+    def get_ESM1v_embeddings(self, accs, seqs):
+        '''Return per-residue embeddings (padded) for protein sequences from ESM1v model'''
+
+        seqs = [utils.replace_noncanonical(seq, 'X') for seq in seqs]
+        data = [(accs[i], seqs[i]) for i in range(len(accs))]
+        batch_labels, batch_strs, batch_tokens = self.esm1v_batch_converter(data)
+        batch_tokens = batch_tokens.to(device=self.device, non_blocking=True)
+        emb = self.esm1v_model(batch_tokens, repr_layers=[33], return_contacts=False)
+        emb = emb["representations"][33]
+        emb = emb.transpose(2,1) # From (batch, seqlen, features) to (batch, features, seqlen)
+
+        return emb
+    
+    
+    def load_RLAT_model(self):
+        '''Return fine-tuned residual light attention top model'''
+
+        # Path to RLAT model
+        this_dir, this_filename = os.path.split(__file__)
+        params_path = os.path.join(this_dir, 'saved_models', 'RLAT', 'params.json')
+        rlat_path = os.path.join(this_dir, 'saved_models', 'RLAT', 'RLAT.pt')
+        
+        # Download RLAT model from google drive if not in path
+        if not os.path.exists(rlat_path):
+            _ = download_models()
+        
+        # Load RLAT model from path
+        checkpoint = torch.load(rlat_path, map_location=self.device)
+        params = utils.read_json(params_path)        
+        model = ResidualLightAttention(**params)
+        model = DataParallel(model)
+        model.load_state_dict(checkpoint['model_state_dict'], strict=True)
+
+        return model
+
+    
+    def batch_predict(self, accs, seqs):
+        '''Predict pHopt with EpHod on a batch of sequences'''
+        
+        emb_esm1v = self.get_ESM1v_embeddings(accs, seqs)
+        maxlen = emb_esm1v.shape[-1]
+        masks = [[1] * len(seqs[i]) + [0] * (maxlen - len(seqs[i])) \
+                 for i in range(len(seqs))]
+        masks = torch.tensor(masks, dtype=torch.int32)
+        output = self.rlat_model(emb_esm1v, masks) # (ypred, emb_ephod, attention_weights)
+        
+        return output
+    
+    
+    
+
